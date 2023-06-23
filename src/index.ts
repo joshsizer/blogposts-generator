25f186473c65123ca6c60bff8a9fa1d8439d5e62
Josh Sizer <joshsizer@gmail.com>
2023-06-22 21:49:30 -0400

    chore: better support env vars

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,40 @@
 import { Configuration, OpenAIApi } from "openai";
+import { exit } from "process";
 
-var trending = "";
+const TRENDING_URL: string = process.env.TRENDING_URL ? process.env.TRENDING_URL as string : ""
+const OPENAI_API_KEY: string = process.env.OPENAI_API_KEY ? process.env.OPENAI_API_KEY as string : ""
+const OPENAI_MODEL: string = process.env.OPENAI_MODEL ? process.env.OPENAI_MODEL as string : "gpt-3.5-turbo"
 
-(async () => {
-    fetch("http://localhost:3000/trending").then(async response => {
-        ({ trending } = await response.json());
-    }).catch(err => {
-        console.log(err)
-    })
-})()
+if (!TRENDING_URL) {
+    console.log("No TRENDING_URL provided. Exiting.")
+    exit(1)
+}
+
+if (!OPENAI_API_KEY) {
+    console.log("No OPENAI_API_KEY provided. Exiting.")
+}
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
-  });
+});
+
 const openai = new OpenAIApi(configuration);
 
-var content = `write a blog post using the topic "${trending}". It should be 2000 words and be written in the style of a blog post. The tone can be similar to a news article, but it is a blog post. The output format of the blog post should be in markdown format. Do not include images. Maintain an upper word count of 2000 words but do not output the word count of each section. Prefer to finish the blog post before your token limit is reached`;
+function getContent(trending: string): string {
+    return `write a blog post using the topic "${trending}". It should be 2000 words and be written in the style of a blog post. The tone can be similar to a news article, but it is a blog post. The output format of the blog post should be in markdown format. Do not include images. Maintain an upper word count of 2000 words but do not output the word count of each section. Prefer to finish the blog post before your token limit is reached`;
+}
 
 (async () => {
+    const response = await fetch(TRENDING_URL);
+    const { trending } = await response.json();
+
+    console.log(`Got value for trending: "${trending}"`)
+    console.log(`Generating blogpost...`)
+
     const chatCompletion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{role: "user", content}],
-      });
+        model: OPENAI_MODEL,
+        messages: [{ role: "user", content: getContent(trending) }],
+    });
     console.log(chatCompletion.data.choices[0].message);
 })()
 
